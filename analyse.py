1301948ed7397374cb571b72294117256e7392f7
Lukas <lukas.faber@student.hpi.de>
2017-01-06 09:21:44 +0100

    analyse.py call all measure

--- analyse.py
+++ analyse.py
@@ -7,7 +7,15 @@ g.remove_vertex(a)
 bv, be = betweenness(g)"""
 
 g = load_graph("beer.graphml")
-print(1)
-a= [node for node in g.vertices() if node.out_degree() + node.in_degree() < 3]
-print(2)
+print("calculate pagerank")
+pr = pagerank(g)
+print("calculate betweenness")
 bv, be = betweenness(g)
+print("calculate closeness")
+cl = closeness(g)
+print("calculate eigenvector centrality")
+ev = eigenvector(g)[1]
+print("calculate katz")
+kz = katz(g)
+print("calculate hits")
+ht = hits(g)
