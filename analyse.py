c3475cc6c889b8ab38f98c71d37491f64a6b218b
Lukas <lukas.faber@student.hpi.de>
2017-01-13 13:59:04 +0100

    changes

--- analyse.py
+++ analyse.py
@@ -6,39 +6,81 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 import tensorflow as tf
+import time
+import sys
 
-g = load_graph("datasets/beer_answers.graphml")
-print("calculate pagerank")
+t0 = time.time()
+g = load_graph(str(sys.argv[1]))
+t1 = time.time()
+print("graph loaded successfully in " + str(t1-t0) + " seconds")
+
+t0 = time.time()
 pr = pagerank(g)
-print("calculate betweenness")
+t1 = time.time()
+print("pagerank calculated in " + str(t1-t0) + " seconds")
+pr = [p for p in pr.a]
+
+t0 = time.time()
 bv, be = betweenness(g)
-print("calculate closeness")
+t1 = time.time()
+print("betweenness calculated in " + str(t1-t0) + " seconds")
+bv = [b for b in bv.a]
+
+t0 = time.time()
 cl = closeness(g)
-print("calculate eigenvector centrality")
+t1 = time.time()
+print("closeness calculated in " + str(t1-t0) + " seconds")
+cl = [c for c in cl.a]
+
+t0 = time.time()
 ev = eigenvector(g)[1]
-print("calculate katz")
+t1 = time.time()
+print("eigenvector centrality calculated in " + str(t1-t0) + " seconds")
+ev = [e for e in ev.a]
+
+t0 = time.time()
 kz = katz(g)
-print("calculate hits")
+t1 = time.time()
+print("katz calculated in " + str(t1-t0) + " seconds")
+kz = [k for k in kz.a]
+
+t0 = time.time()
 ht = hits(g)
+t1 = time.time()
+print("hits calculated in " + str(t1-t0) + " seconds")
+auth = [h for h in ht[1].a]
+hub = [h for h in ht[2].a]
 
-betweenness = np.asarray([be for be in bv.a])
-betweenness = np.asarray([v.out_degree() for v in g.vertices()])
-reputation = np.asarray([rep for rep in g.vertex_properties["Reputation"]])
-#reputation = reputation / max(reputation)
+t0 = time.time()
+id = [v.in_degree() for v in g.vertices()]
+t1 = time.time()
+print("in degree calculated in " + str(t1-t0) + " seconds")
 
-X = np.asarray([[be for be in bv.a],
-	[v.out_degree() for v in g.vertices()],
-	[v.in_degree() for v in g.vertices()],
-	[p for p in pr.a],
-	#[c for c in cl.a],
-	[e for e in ev.a],
-	[k for k in kz.a],
-	[h for h in ht[1].a],
-	[h for h in ht[2].a]
-]).transpose()
+t0 = time.time()
+od = [v.out_degree() for v in g.vertices()]
+t1 = time.time()
+print("out degree calculated in " + str(t1-t0) + " seconds")
 
-plt.scatter(betweenness,reputation)
-#plt.show()
+rep = np.asarray([rep for rep in g.vertex_properties["Reputation"]])
+
+
+X = np.asarray([
+    [1 for b in bv],
+    pr, bv, ev, kz, auth, hub, id, od
+])
+
+for feature in X:
+    print(len(feature))
+    print(sum(feature==0))
+    plt.scatter(feature, rep)
+    plt.show()
+
+for feature in X[1:,]:
+    print(np.corrcoef(feature,rep))
+
+
+X = X[filt]
+reputation = reputation[filt]
 """
 model = Sequential()
 model.add(Dense(13, input_dim=1, init='normal', activation='relu'))
@@ -49,20 +91,5 @@ model.fit(betweenness[:350], reputation[:350], nb_epoch=1000)
 print("result")
 print(model.evaluate(betweenness[350:], reputation[350:]))
 """
-w = tf.cast(tf.Variable(tf.zeros([8,1])),tf.float64)
-b = tf.cast(tf.Variable(tf.zeros([1])), tf.float64)
-mod = tf.matmul(X,w) + b
-loss = tf.reduce_mean(tf.square(reputation-mod))
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
 
-init = tf.global_variables_initializer()
-
-sess = tf.Session()
-sess.run(init)
-for step in range(201):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(w), sess.run(b))
-prediction = sess.run(mod, betweenness.all())
-print(prediction)
+w = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(),X)),X.transpose()),reputation)
